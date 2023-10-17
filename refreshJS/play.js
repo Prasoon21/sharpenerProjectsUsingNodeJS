dae8afa1fc4f4b1c7dabd34d9c0ea6397930d5f9
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-17 16:22:39 +0530

    Used fat arrow and object

---
+++ refreshJS/play.js
@@ -0,0 +1,15 @@
+// const product = (a,b) => a*b;
+
+
+// console.log(product(2,3));
+
+
+const student = {
+    name: 'Rahul',
+    age: 19,
+    greet(){
+        console.log('Hey, I am ' + this.name);
+    }
+};
+
+student.greet();
